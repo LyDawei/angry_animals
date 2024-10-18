3b671fdd02265488be56243f94d0b4d2e6841e34
lydawei <lydawei@duck.com>
2024-10-18 00:31:02 -0500

    made progress on animation, audio, and play mechanics
    bug report: weird bug issue with mouse positioning...

--- scenes/animal/animal.gd
+++ scenes/animal/animal.gd
@@ -1,20 +1,26 @@
 extends RigidBody2D
 
 enum ANIMAL_STATE {READY, DRAG, RELEASE}
-@onready var label: Label = $AnimalState
-@onready var vector_2_debug: Label = $Vector2_Debug
-@onready var stretch_sound: AudioStreamPlayer2D = $Stretch_Sound
-@onready var arrow: Sprite2D = $Arrow
+@onready var label: Label 						= $AnimalState
+@onready var vector_2_debug: Label 				= $Vector2_Debug
+@onready var stretch_sound: AudioStreamPlayer2D 	= $StretchSound
+@onready var launch_sound: AudioStreamPlayer2D 	= $LaunchSound
+@onready var arrow: Sprite2D 					= $Arrow
+@onready var collision_sound: AudioStreamPlayer2D = $CollisionSound
 
 
-const DRAG_LIM_MAX = Vector2(0,60)
-const DRAG_LIM_MIN = Vector2(-60,0)
+const DRAG_LIM_MAX: Vector2 	= Vector2(0,60)
+const DRAG_LIM_MIN: Vector2 	= Vector2(-60,0)
+const IMPULSE_MULT: float  	= 20.0
+const IMPULSE_MAX: 	float	= 1200.0
 
-var _state:ANIMAL_STATE = ANIMAL_STATE.READY
-var _start:Vector2 = Vector2.ZERO
-var _drag_start:Vector2 = Vector2.ZERO
-var _dragged_vector:Vector2 = Vector2.ZERO
-var _last_dragged_vector: Vector2 = Vector2.ZERO
+var _state: ANIMAL_STATE 			= ANIMAL_STATE.READY
+var _start: Vector2 					= Vector2.ZERO
+var _drag_start: Vector2 			= Vector2.ZERO
+var _dragged_vector: Vector2 		= Vector2.ZERO
+var _last_dragged_vector: Vector2 	= Vector2.ZERO
+var _arrow_scale_x: float			= 0.0
+var _last_collision_count:int		= 0
 
 func detect_release()->bool:
 	if _state == ANIMAL_STATE.DRAG:
@@ -27,7 +33,7 @@ func detect_release()->bool:
 # This gives us a reference point of what the "dragged vector" is.
 func get_dragged_vector(gmp:Vector2)->Vector2:
 	return gmp - _drag_start
-	
+
 func drag_in_limits()  -> void:
 	_last_dragged_vector = _dragged_vector
 	_dragged_vector.x = clampf(
@@ -55,16 +61,29 @@ func update_drag()->void:
 	drag_in_limits()
 	scale_arrow()
 	pass
+	
+func update_flight()->void:
+	if (
+		_last_collision_count == 0 and
+		get_contact_count() > 0 and
+		!collision_sound.playing
+		):
+		collision_sound.play()
+	_last_collision_count = get_contact_count()
 
 func update(delta:float) -> void:
 	if _state == ANIMAL_STATE.DRAG:
 		update_drag()
+	if _state == ANIMAL_STATE.RELEASE:
+		update_flight()
 
 func set_new_state(new_state: ANIMAL_STATE)->void:
 	_state = new_state
 	if _state == ANIMAL_STATE.RELEASE:
 		freeze = false
 		arrow.hide()
+		launch_sound.play()
+		apply_central_impulse(get_impulse())
 	elif _state == ANIMAL_STATE.DRAG:
 		freeze = true
 		arrow.show()
@@ -73,16 +92,32 @@ func handle_animal_died() -> void:
 	queue_free()
 
 func scale_arrow() -> void:
+	var imp_len = get_impulse().length()
+	var perc = imp_len/IMPULSE_MAX
+	
+	arrow.scale.x = (_arrow_scale_x * perc) + _arrow_scale_x
 	#The arrow points at starting position. It does this by using the math below.
 	arrow.rotation = (_start-position).angle()
+	
 	pass
 
 func play_stretch_sound() -> void:
 	if (_last_dragged_vector - _dragged_vector).length() > 0 and !stretch_sound.playing:
 		stretch_sound.play()
 
+func get_impulse() -> Vector2:
+	return _dragged_vector * -1 * IMPULSE_MULT
+
+func die() -> void:
+	SignalManager.on_animal_died.emit()
+
+func on_animal_collide_platform()->void:
+	if get_contact_count() == 1:
+		collision_sound.play()
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	_arrow_scale_x = arrow.scale.x
 	SignalManager.on_animal_died.connect(handle_animal_died)
 	_start = position
 	pass # Replace with function body.
@@ -94,9 +129,17 @@ func _physics_process(delta: float) -> void:
 	vector_2_debug.text = '%.1f, %.1f' % [_dragged_vector.x, _dragged_vector.y]
 
 func _on_animal_exited() -> void:
-	SignalManager.on_animal_died.emit()
-	pass # Replace with function body.
+	die()
 
 func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
 	if (_state == ANIMAL_STATE.READY and event.is_action_pressed('drag')):
 		set_new_state(ANIMAL_STATE.DRAG)
+
+
+func _on_sleeping_state_changed() -> void:
+	if sleeping:
+		var colliding_bodies = get_colliding_bodies()
+		if colliding_bodies.size() > 0:
+			colliding_bodies[0].die()
+		call_deferred('die')
+	pass # Replace with function body.
--- signal_manager.gd
+++ signal_manager.gd
@@ -2,3 +2,5 @@ extends Node
 
 signal on_water_collide
 signal on_animal_died
+signal on_attempt_made
+signal on_cup_destroyed
